5b5617fd91a8d72c4932de665c8f0db8d7920efe
Sebastian Windeck <sebastian.windeck@web.de>
2018-12-11 21:22:02 +0100

    Update LSTM models

---
+++ main_lstm_basic.py
@@ -0,0 +1,81 @@
+import os
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.layers import Input
+from keras.layers import LSTM
+from keras.models import Model
+from keras.utils import plot_model, to_categorical
+from sklearn.metrics import confusion_matrix
+
+from helpers.plotter import plot_confusion_matrix
+
+
+def build(base_shape):
+    inputer = Input(shape=base_shape, name='input')
+    lstm = LSTM(3, return_sequences=True, stateful=False, recurrent_activation='hard_sigmoid', name='lstm_eeg1')(inputer)
+
+    _model = Model(inputs=inputer, outputs=lstm)  # type: Model
+    return _model
+
+
+eeg1 = np.load(os.getcwd() + '/data/numpy/data_eeg1.npy')
+print(eeg1.shape)
+eeg2 = np.load(os.getcwd() + '/data/numpy/data_eeg2.npy')
+print(eeg2.shape)
+emg = np.load(os.getcwd() + '/data/numpy/data_emg.npy')
+print(emg.shape)
+lab = np.load(os.getcwd() + '/data/numpy/labels.npy')
+
+eeg1 = np.mean(eeg1, axis=2)
+eeg2 = np.mean(eeg2, axis=2)
+emg = np.mean(emg, axis=2)
+
+lab = np.subtract(lab, 1)
+print("Unique labels: ", np.unique(lab))
+encoded_lab = to_categorical(lab, num_classes=3)
+
+data = np.concatenate((eeg1, eeg2, emg), axis=2)
+
+base_shape = (data.shape[1], data.shape[2])
+print('Input shape: ', base_shape)
+print('Label shape: ', encoded_lab.shape)
+print('Input done.')
+
+model = build(base_shape)
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['categorical_accuracy'])
+
+print(model.summary())
+plot_model(model, to_file=os.getcwd() + '/data/' + str(time.strftime("%Y%m%d-%H%M%S")) + '_model.png', show_shapes=True, show_layer_names=True, rankdir='TB')
+
+
+model.fit(data, encoded_lab, batch_size=1, epochs=20, verbose=1,
+          callbacks=None)
+
+y_pred = model.predict(data)
+
+print(y_pred[1, 1, :])
+y_pred = np.argmax(y_pred, axis=2)
+y_pred = np.add(y_pred, 1)
+
+plt.plot(y_pred[0])
+plt.plot(y_pred[1])
+plt.plot(y_pred[2])
+plt.show()
+
+cnf_matrix = confusion_matrix(np.reshape(lab, (64800,)), np.reshape(y_pred, (64800,)))
+np.set_printoptions(precision=2)
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=[0, 1, 2],
+                      title='Confusion matrix, without normalization')
+
+# Plot normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=[0, 1, 2], normalize=True,
+                      title='Normalized confusion matrix')
+
+plt.show()
--- main_lstm_stateful.py
+++ main_lstm_stateful.py
@@ -13,35 +13,29 @@ from helpers.plotter import plot_confusion_matrix
 
 
 def build(base_shape):
-    eeg1_inputer = Input(shape=base_shape, name='eeg1_input')
-    eeg2_inputer = Input(shape=base_shape, name='eeg2_input')
-    emg_inputer = Input(shape=base_shape, name='emg_input')
+    inputer = Input(batch_shape=base_shape, name='input')
+    lstm = LSTM(3, return_sequences=False, stateful=True, recurrent_activation='hard_sigmoid', name='lstm_state')(inputer)
 
-    lstm_out1 = LSTM(64, return_sequences=True, recurrent_activation='hard_sigmoid', name='lstm_eeg1', stateful=False)(eeg1_inputer)
-    lstm_out2 = LSTM(64, return_sequences=True, recurrent_activation='hard_sigmoid', name='lstm_eeg2', stateful=False)(eeg2_inputer)
-    lstm_out3 = LSTM(64, return_sequences=True, recurrent_activation='hard_sigmoid', name='lstm_emg', stateful=False)(emg_inputer)
-
-    merged = concatenate([lstm_out1, lstm_out2, lstm_out3], axis=2, name='merger')
-
-    lstm_merged = LSTM(3, activation='softmax', recurrent_activation='hard_sigmoid', name='lstm_merged', return_sequences=True, stateful=False)(merged)
-
-    _model = Model(inputs=[eeg1_inputer, eeg2_inputer, emg_inputer], outputs=lstm_merged)  # type: Model
+    _model = Model(inputs=inputer, outputs=lstm)  # type: Model
     return _model
 
 
 eeg1 = np.load(os.getcwd() + '/data/numpy/data_eeg1.npy')
+print(eeg1.shape)
 eeg2 = np.load(os.getcwd() + '/data/numpy/data_eeg2.npy')
 emg = np.load(os.getcwd() + '/data/numpy/data_emg.npy')
 lab = np.load(os.getcwd() + '/data/numpy/labels.npy')
 
-eeg1 = np.mean(eeg1, axis=2)
-eeg2 = np.mean(eeg2, axis=2)
-emg = np.mean(emg, axis=2)
-
+eeg1 = eeg1[0]
+eeg2 = eeg2[0]
+emg = emg[0]
+data = np.concatenate((eeg1,eeg2,emg),axis=2)
+lab = lab[0]
 lab = np.subtract(lab, 1)
-encoded_lab= to_categorical(lab, num_classes=None)
 
-base_shape = (eeg1.shape[1], eeg1.shape[2])
+encoded_lab= to_categorical(lab, num_classes=3)
+
+base_shape = (1, data.shape[1], data.shape[2])
 print('Input shape: ', base_shape)
 print('Label shape: ', encoded_lab.shape)
 print('Input done.')
@@ -55,23 +49,22 @@ print(model.summary())
 plot_model(model, to_file=os.getcwd() + '/data/' + str(time.strftime("%Y%m%d-%H%M%S")) + '_model.png', show_shapes=True, show_layer_names=True, rankdir='TB')
 
 print("Unique labels: ", np.unique(lab))
-model.fit({'eeg1_input': eeg1, 'eeg2_input': eeg2, 'emg_input': emg}, encoded_lab, batch_size=1, epochs=10, verbose=1,
+model.fit(data, encoded_lab, batch_size=1, epochs=100, verbose=1,
           callbacks=None)
 
-y_pred = model.predict({'eeg1_input': eeg1, 'eeg2_input': eeg2, 'emg_input': emg})
+y_pred = model.predict(data, batch_size=1, verbose=1)
 
-print(y_pred[1,1,:])
-y_pred = np.argmax(y_pred, axis=2)
+print(y_pred[1,:])
+y_pred = np.argmax(y_pred, axis=1)
 y_pred = np.add(y_pred, 1)
 
 
-plt.plot(y_pred[0])
-plt.plot(y_pred[1])
-plt.plot(y_pred[2])
+plt.plot(y_pred)
+
 plt.show()
 
 
-cnf_matrix = confusion_matrix(np.reshape(lab,(64800,)), np.reshape(y_pred,(64800,)))
+cnf_matrix = confusion_matrix(np.reshape(lab,(21600,)), np.reshape(y_pred,(21600,)))
 np.set_printoptions(precision=2)
 
 # Plot non-normalized confusion matrix
