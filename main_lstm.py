46ad85a6b352a6e7e9895c05996e5dfb3bce31e1
Sebastian Windeck <sebastian.windeck@web.de>
2018-12-11 15:41:40 +0100

    Update main_lstm.py

--- main_lstm.py
+++ main_lstm.py
@@ -5,17 +5,9 @@ from sklearn.metrics import confusion_matrix
 from keras.layers import LSTM
 from keras.models import Model
 from keras.layers import Input, Dense, Concatenate
+from keras.utils import plot_model
 
 from helpers.plotter import plot_confusion_matrix
-
-eeg1 = np.load(os.getcwd() + '/data/numpy/data_eeg1.npy')
-eeg2 = np.load(os.getcwd() + '/data/numpy/data_eeg2.npy')
-emg = np.load(os.getcwd() + '/data/numpy/data_emg.npy')
-lab = np.load(os.getcwd() + '/data/numpy/labels.npy')
-
-shaper = eeg1.shape
-print('Shape: ', shaper)
-print('Input done.')
 
 
 def build(base_shape):
@@ -23,23 +15,39 @@ def build(base_shape):
     eeg2_inputer = Input(shape=base_shape, name='eeg2_input')
     emg_inputer = Input(shape=base_shape, name='emg_input')
 
-    lstm_out1 = LSTM(32)(eeg1_inputer)
-    lstm_out2 = LSTM(32)(eeg2_inputer)
-    lstm_out3 = LSTM(32)(emg_inputer)
+    lstm_out1 = LSTM(32, return_sequences=False)(eeg1_inputer)
+    lstm_out2 = LSTM(32, return_sequences=False)(eeg2_inputer)
+    lstm_out3 = LSTM(32, return_sequences=False)(emg_inputer)
 
-    merged = Concatenate([lstm_out1, lstm_out2, lstm_out3])
+    merged = Concatenate([lstm_out1, lstm_out2, lstm_out3], axis=-1)
     outputer = Dense(21600, activation='tanh', name='output')(merged)
 
     _model = Model(inputs=[eeg1_inputer, eeg2_inputer, emg_inputer], outputs=outputer)  # type: Model
     return _model
 
 
-model = build(shaper)
+eeg1 = np.load(os.getcwd() + '/data/numpy/data_eeg1.npy')
+eeg2 = np.load(os.getcwd() + '/data/numpy/data_eeg2.npy')
+emg = np.load(os.getcwd() + '/data/numpy/data_emg.npy')
+lab = np.load(os.getcwd() + '/data/numpy/labels.npy')
+
+
+eeg1 = np.mean(eeg1,axis=2)
+eeg2 = np.mean(eeg2,axis=2)
+emg = np.mean(emg,axis=2)
+
+
+base_shape = (eeg1.shape[1],eeg1.shape[2])
+print('Shape: ', base_shape)
+print('Input done.')
+
+model = build(base_shape)
 model.compile(loss='sparse_categorical_crossentropy',
               optimizer='adam',
               metrics=['sparse_categorical_accuracy'])
 
 print(model.summary())
+plot_model(model, to_file='model.png', show_shapes=False, show_layer_names=True, rankdir='TB')
 
 labels = np.subtract(lab, 1)
 print("Unique labels: ", np.unique(labels))
