72726776133ff55af4d59f7c38f15d5b575a1a4f
Sebastian Windeck <sebastian.windeck@web.de>
2018-12-11 15:09:03 +0100

    Update convert_to_npy.py

--- convert_to_npy.py
+++ convert_to_npy.py
@@ -7,16 +7,16 @@ train_emg = np.genfromtxt('data/import/train_eeg1.csv', delimiter=',', skip_head
 train_labels = np.genfromtxt('data/import/train_labels.csv', delimiter=',', skip_header=1)
 print("Imported everything.")
 
-train_eeg1 = train_eeg1[:,1:]
-train_eeg2 = train_eeg2[:,1:]
-train_emg = train_emg[:,1:]
-train_labels = train_labels[:,1:]
+train_eeg1 = train_eeg1[:, 1:]
+train_eeg2 = train_eeg2[:, 1:]
+train_emg = train_emg[:, 1:]
+train_labels = train_labels[:, 1:]
 
-train_eeg1 = np.reshape(train_eeg1,(3,-1,4,128))
+train_eeg1 = np.reshape(train_eeg1, (3, -1, 4, 128))
 print("Train shape: ", train_eeg1.shape)
-train_eeg2 = np.reshape(train_eeg2,(3,-1,4,128))
-train_emg = np.reshape(train_emg,(3,-1,4,128))
-train_labels = np.reshape(train_labels, (3,-1))
+train_eeg2 = np.reshape(train_eeg2, (3, -1, 4, 128))
+train_emg = np.reshape(train_emg, (3, -1, 4, 128))
+train_labels = np.reshape(train_labels, (3, -1))
 print("Reshape it.")
 print("Mean it.")
 
@@ -28,5 +28,3 @@ np.save(path + '/data/numpy/data_emg.npy', train_emg)
 np.save(path + '/data/numpy/labels.npy', train_labels)
 
 print('Exported everything')
-
-
